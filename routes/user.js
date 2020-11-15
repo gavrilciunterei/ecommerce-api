4fc10b1f889d1606104ccf0841693597a8d8008e
gavrilciunterei <gavriltaller@gmail.com>
2020-11-15 14:51:04 +0200

    Added express validator

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/user');
+const { userSignupValidator } = require('../validator/index');
 
-router.post('/signup', controller.signup);
+router.post('/signup', userSignupValidator, controller.signup);
 
 module.exports = router;
