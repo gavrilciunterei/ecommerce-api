809bd2d12685c55e73a0f890dde7adfb989d043c
gavrilciunterei <gavriltaller@gmail.com>
2020-11-19 13:25:43 +0200

    Added delete, get by id and update  product

--- controllers/product.js
+++ controllers/product.js
@@ -14,8 +14,111 @@ var controller = {
           error: 'Image could not be uploaded',
         });
       }
+
+      // check for all fields
+      const { name, description, price, category, quantity, shipping } = fields;
+
+      if (
+        !name ||
+        !description ||
+        !price ||
+        !category ||
+        !quantity ||
+        !shipping
+      ) {
+        return res.status(400).json({
+          error: 'All fields are required',
+        });
+      }
+
       let product = new Product(fields);
       if (files.photo) {
+        // Restrict file size
+        if (files.photo.size > 1000000) {
+          return res.status(400).json({
+            error: 'Image should be less than 1mb in size',
+          });
+        }
+        product.photo.data = fs.readFileSync(files.photo.path);
+        product.photo.contentType = files.photo.type;
+      }
+      product.save((err, result) => {
+        if (err) {
+          return res.status(400).json({
+            err: errorHandler(err),
+          });
+        }
+        res.json(result);
+      });
+    });
+  },
+  productById: function (req, res, next, id) {
+    Product.findById(id).exec((err, product) => {
+      if (err || !product) {
+        return res.status(400).json({
+          error: 'Product not found',
+        });
+      }
+      req.product = product;
+      next();
+    });
+  },
+
+  read: function (req, res) {
+    req.product.photo = undefined;
+    return res.json(req.product);
+  },
+
+  remove: function (req, res) {
+    let product = req.product;
+    product.remove((err, deleteProduct) => {
+      if (err) {
+        return res.status(400).json({
+          err: errorHandler(err),
+        });
+      }
+      res.json({
+        message: 'Product deleted successfully',
+      });
+    });
+  },
+
+  update: function (req, res) {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return res.status(400).json({
+          error: 'Image could not be uploaded',
+        });
+      }
+
+      // check for all fields
+      const { name, description, price, category, quantity, shipping } = fields;
+
+      if (
+        !name ||
+        !description ||
+        !price ||
+        !category ||
+        !quantity ||
+        !shipping
+      ) {
+        return res.status(400).json({
+          error: 'All fields are required',
+        });
+      }
+
+      let product = req.product;
+      product = _.extend(product, fields);
+
+      if (files.photo) {
+        // Restrict file size
+        if (files.photo.size > 1000000) {
+          return res.status(400).json({
+            error: 'Image should be less than 1mb in size',
+          });
+        }
         product.photo.data = fs.readFileSync(files.photo.path);
         product.photo.contentType = files.photo.type;
       }
--- routes/product.js
+++ routes/product.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
+router.get('/product/:productId', controller.read);
 router.post(
   '/product/create/:userId',
   requireSignin,
@@ -11,7 +12,22 @@ router.post(
   isAdmin,
   controller.create
 );
+router.delete(
+  '/product/:productId/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  controller.remove
+);
+router.put(
+  '/product/:productId/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  controller.update
+);
 
 router.param('userId', userById);
+router.param('productId', controller.productById);
 
 module.exports = router;
